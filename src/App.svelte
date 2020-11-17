e0944ca28694131f8247d6a9c2fefb2de17d8986
Vlad Podvorny <vpodvorny@gmail.com>
2020-11-17 15:43:36 -0500

    configuration and demo page

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,35 @@
 <script>
-	export let name;
+  export let name;
 </script>
 
-<style global lang="scss">
-	h1 {
-		color: purple;
+<style global lang="less">
+  @import "./styles/global.less";
+
+  main {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    .font-regular();
+    font-size: 2 * @fs-base;
+    color: #777;
+    letter-spacing: @fs-base;
 
     span {
-      color: green;
+      color: @c-red;
+      text-transform: uppercase;
     }
-	}
+  }
 </style>
 
-<h1>Hello <span>{name}</span>!</h1>
+<main>
+  <h1>Hello <span>{name}</span>!</h1>
+</main>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
-const path = require('path');
-const  { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const  DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
-const  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const preprocess = require("svelte-preprocess");
-const  HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const distDir = path.resolve(__dirname, 'public');
-const  srcDir = path.resolve(__dirname, 'src');
+const srcDir = path.resolve(__dirname, "src");
+const distDir = path.resolve(__dirname, "public");
 
-const IS_DEV = process.env.NODE_ENV === 'development';
-const MODE = IS_DEV ? 'development' : 'production';
+const IS_DEV = process.env.NODE_ENV === "development";
+const MODE = IS_DEV ? "development" : "production";
 
 module.exports = {
-  target: 'web',
+  target: "web",
   mode: MODE,
   entry: {
     bundle: ["./src/main.js"],
@@ -21,15 +20,14 @@ module.exports = {
     alias: {
       svelte: path.resolve("node_modules", "svelte"),
     },
-    extensions: [".mjs", ".js", '.scss', ".svelte"],
+    extensions: [".mjs", ".js", ".scss", ".svelte"],
     mainFields: ["svelte", "browser", "module", "main"],
 
-    modules: ['node_modules'],
-    plugins: [new DirectoryNamedWebpackPlugin(true)]
+    modules: ["node_modules"],
+    plugins: [new DirectoryNamedWebpackPlugin(true)],
   },
 
   output: {
-    //path: __dirname + "/public",
     path: distDir,
     filename: "[name].js",
     chunkFilename: "[id].js",
@@ -42,71 +40,86 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'svelte-loader',
+            loader: "svelte-loader",
             options: {
               emitCss: !IS_DEV,
               hotReload: IS_DEV,
               preprocess: preprocess({
                 postcss: true,
-              })
-            }
-          }
-        ]
+              }),
+            },
+          },
+        ],
       },
 
       {
         test: /\.css$/,
         use: [
-          { loader: IS_DEV ? 'style-loader' : MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader' }
-        ]
+          { loader: IS_DEV ? "style-loader" : MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+        ],
       },
       {
         test: /\.s[ac]ss$/,
         use: [
-          { loader: IS_DEV ? 'style-loader' : MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader' },
+          { loader: IS_DEV ? "style-loader" : MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sassOptions: {
-                outputStyle: IS_DEV ? 'expanded' : 'compressed',
+                outputStyle: IS_DEV ? "expanded" : "compressed",
                 precision: 8,
-                includePaths: [path.resolve(srcDir, 'styles')]
-              }
-            }
-          }
-        ]
-      }
-    ]
+                includePaths: [path.resolve(srcDir, "styles")],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: IS_DEV ? "style-loader" : MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                includePath: [path.resolve(srcDir, "styles")],
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
-  
+
   plugins: [
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
 
     new HtmlWebpackPlugin({
-      title: 'Svelte App',
+      title: "Svelte App",
       version: Math.random().toString(32).slice(2, 10),
-      chunks: ['index'],
-      filename: 'index.html',
-      template: path.resolve(srcDir, 'template.html')
+      chunks: ["index"],
+      filename: "index.html",
+      template: path.resolve(srcDir, "template.html"),
     }),
-
   ],
+
   watchOptions: {
-    ignored: /node_modules/
+    ignored: /node_modules/,
   },
-  devtool: IS_DEV ? 'cheap-module-eval-source-map' : 'source-map',
+  devtool: IS_DEV ? "cheap-module-eval-source-map" : "source-map",
   //devtool: prod ? false : "source-map",
   devServer: {
     contentBase: distDir,
     compress: true,
-    port: 5000
-  }
+    port: 5000,
+  },
 };
